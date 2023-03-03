216d9ee60fe2cf3799152e11c116ef95261d8578
Luigi Ju <7v7luitopvp@gmail.com>
2023-03-03 01:46:17 -0500

    build: :wrench: added script and change package

--- config.d.ts
+++ config.d.ts
@@ -2,6 +2,13 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			PORT: string
+			DATABASE_TYPE: string
+			DATABASE_HOST: string
+			DATABASE_USERNAME: string
+			DATABASE_PASSWORD: string
+			DATABASE_NAME: string
+			DATABASE_PORT: string
+			DATABASE_SSL: string
 		}
 	}
 }
